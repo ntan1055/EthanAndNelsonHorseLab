0f767e304680c0de6a733c43702ee41eda2712ce
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2018-11-19 08:42:03 -0500

    Update Mustang.java

--- src/com/company/Mustang.java
+++ src/com/company/Mustang.java
@@ -4,10 +4,12 @@ public class Mustang implements Horse
 {
     private String name;
     private int weight;
+    private int spaces;
     public Mustang(String name, int weight)
     {
         this.name = name;
         this.weight = weight;
+        this.spaces = spaces;
     }
     public String getName()
     {
@@ -16,5 +18,15 @@ public class Mustang implements Horse
     public int getWeight()
     {
         return weight;
+    }
+    public int getSpaces()
+    {
+        return spaces;
+    }
+    public String toString()
+    {
+        String output = "";
+        output += this.getName() + "," + " weighs: " + this.getWeight();
+        return output;
     }
 }
